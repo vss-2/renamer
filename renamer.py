aa17e6768819a708a6fa6bfe1621d2fd6ae8846b
zegabr <jgsp2@cin.ufpe.br>
2018-02-10 15:29:31 -0300

    few simplifications

--- renamer.py
+++ renamer.py
@@ -1,33 +1,32 @@
 from pathlib import Path
 
-def walk(dir,newName,extension):
+def walk(dir,extension):
     dirs = [x for x in dir.iterdir() if x.is_dir()]#list of directories
     for d in dirs:
-        walk(d,newName,extension)
-        renameNow(d,newName,extension)#when there is no direcroties below
-        
+        walk(d,extension)
+        renameNow(d,extension)#when there is no direcroties below
+#################################################        
     
-def renameNow(dir,newName,extension):# rename all files in dir directory
+def renameNow(dir,extension):# rename all files in dir directory
     t = dir.parts
     
-    #removing repetitions in t
+#######removing repetitions in t
     seen = []
     answer = []
     for elem in t:
         if elem not in seen:
             seen.append(elem)
             answer.append(elem)
-    #repetitions removed
+######repetitions removed
+
+#############
     n = len(answer)-1
-    
-    files = list(dir.glob('./*'+extension))##lista com todos os arquivos com a mesma extensao dada deste diretorio e dos diretorios dentro deles(merda)
-    
+    newName=''
     while(n>=0):
         newName = answer[n]+'_'+ newName
         n=n-1
-    
-    print('\ndirectory: '+newName+'\nfile:')
-    
+############# 
+    files = list(dir.glob('./*'+extension))##lista com todos os arquivos com a mesma extensao dada deste diretorio
     i=0
     for file in files:
         i=i+1
@@ -35,18 +34,17 @@ def renameNow(dir,newName,extension):# rename all files in dir directory
         print(file)##arquivo a ser renomeado
         print('newname'+finalName)##novo nome do arquivo
         file.rename(str(dir)+'/'+finalName)
-        
+##############################################       
         
     
 
-
+#####   main script   ####
 p = Path('.')## path to this directory
-newName = '' #dont touch here
 extension = ".jpg" # change extension here
 print("trocando nome dos arquivos "+extension+'...')
 
-walk(p,'',extension)
+walk(p,extension)
 
-print('\npronto')
+print('\nPRONTO.')
 
 
