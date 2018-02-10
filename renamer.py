b34a6a8ca878e0d716e232d39849f9d2c5558dc4
zegabr <jgsp2@cin.ufpe.br>
2018-02-09 21:35:57 -0300

    Recursion working, but too much. There is a bug in line 29, it should get only the files in the current directory, but it is getting the files in directories below as well

--- renamer.py
+++ renamer.py
@@ -1,40 +1,51 @@
 from pathlib import Path
 
-def walk(dir,previousname,extension):
+def walk(dir,newName,extension):
     dirs = [x for x in dir.iterdir() if x.is_dir()]#list of directories
     for d in dirs:
-        t = d.parts
-        n = len(t)
-        dirname = t[n-1]
-        print(dirname)
-        print
-        if(previousname!=''):
-            newName = previousname+'_' +dirname
-        else:
-            newName = dirname
         walk(d,newName,extension)
-        renameNow(dir/d,newName,extension)#when there is no direcroties below
+        renameNow(d,newName,extension)#when there is no direcroties below
         
     
 def renameNow(dir,newName,extension):# rename all files in dir directory
-    files = list(dir.glob('**/*.'+extension))##lista com todos os arquivos com a mesma extensao dada deste diretorio
-    #print(files)
+    t = dir.parts
+    
+    #removing repetitions in t
+    seen = []
+    answer = []
+    for elem in t:
+        if elem not in seen:
+            seen.append(elem)
+            answer.append(elem)
+    #repetitions removed
+    
+    
+    n = len(answer)-1
+    while(n>=0):
+        newName = answer[n]+'_'+ newName
+        n=n-1
+    
+    print('\ndirectory: '+newName)
+    files = list(dir.glob('**/*'+extension))##lista com todos os arquivos com a mesma extensao dada deste diretorio e dos diretorios dentro deles(merda)
     i=0
     for file in files:
         i=i+1
         finalName = newName+str(i)+extension
+        print(file)##arquivo a ser renomeado
+        print(finalName)##novo nome do arquivo
         file.rename(str(dir)+'/'+finalName)
-        #print(finalName)
+        
+        
     
 
 
 p = Path('.')## path to this directory
 newName = '' #dont touch here
-extension = "jpg" # change extension here
+extension = ".jpg" # change extension here
 print("trocando nome dos arquivos "+extension+'...')
 
-walk(p,newName,extension)
+walk(p,'',extension)
 
-print('\n\npronto')
+print('\npronto')
 
 
